b1fdb3f216ca8b15919dbee2b0c5948d5ac560e0
Brian Rui <Brian.Rui01@gmail.com>
2022-04-24 13:14:20 -0400

    Update skeleton code

--- main_CNN.py
+++ main_CNN.py
@@ -1,8 +1,26 @@
 import model_CNN
 
+
+def train():
+    # TODO: Train the model
+    raise NotImplementedError()
+
+
+def predict():
+    # TODO: Predict
+    raise NotImplementedError()
+
+
 if __name__ == "__main__":
     arguments = get_args(sys.argv)
-    MODE = arguments.get('mode')
+    MODE = arguments.get("mode")
 
-    # TODO: Implement
-    raise NotImplementedError()
+    # Use CUDA if available
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    if MODE == "train":
+        train()
+    elif MODE == "predict":
+        predict()
+    else:
+        raise Exception("Mode not recognized")

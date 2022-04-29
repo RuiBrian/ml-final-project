b221f2eab3c1d89a50bbd4b15fffeb357a91bf30
Brian Rui <Brian.Rui01@gmail.com>
2022-04-28 20:02:18 -0400

    Use CUDA if available

--- main_CNN.py
+++ main_CNN.py
@@ -6,11 +6,8 @@ import sys
 from torchsummary import summary
 
 
-def train():
+def train(device):
     TRAIN_SEQUENCES = np.load("datasets/processed/train_encoded.npy")
-    # TRAIN_SEQUENCES = (TRAIN_SEQUENCES - TRAIN_SEQUENCES.mean()) / (
-    #     TRAIN_SEQUENCES.std()
-    # )
     TRAIN_LABELS = np.load("datasets/processed/train_labels.npy")
 
     # Number of gene sequences in the training corpus
@@ -29,24 +26,23 @@ def train():
 
     # Initialize model and optimizer
     model = CNN(input_height=HEIGHT, input_width=WIDTH, n_classes=N_CLASSES)
+    model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-    
-    s = 0
+
+    cur_label_index = 0
     for step in range(0, EPOCHS, HEIGHT):
-        x = torch.from_numpy(TRAIN_SEQUENCES[step : step + HEIGHT].astype(np.float32))
-        y = torch.from_numpy(TRAIN_LABELS[s].astype(int))
-        s += 1
-            
+        x = torch.from_numpy(
+            TRAIN_SEQUENCES[step : step + HEIGHT].astype(np.float32)
+        ).to(device)
+        y = torch.from_numpy(TRAIN_LABELS[cur_label_index].astype(int)).to(device)
+        cur_label_index += 1
+
         # Forward pass: get logits for x
         logits = model(x)
         logits_label = torch.softmax(logits, 1)
 
         # Compute loss
         loss = F.cross_entropy(logits, y)
-        
-        # if step > 100*82:
-            # print(torch.softmax(logits, 1))
-            # print(torch.max(logits_label, 1)[1].item())
 
         # Zero gradients, perform a backward pass, and update the weights.
         optimizer.zero_grad()
@@ -56,25 +52,24 @@ def train():
     torch.save(model, "models/CNN.pt")
 
 
-def predict():
-    TEST_SEQUENCES = np.load("datasets/processed/test_encoded.npy")
-    # TEST_SEQUENCES = (TEST_SEQUENCES - TEST_SEQUENCES.mean()) / (TEST_SEQUENCES.std())
+def predict(device):
+    DEV_SEQUENCES = np.load("datasets/processed/dev_encoded.npy")
     HEIGHT = 82
-    NUM_SEQUENCES = int(TEST_SEQUENCES.shape[0] / HEIGHT)
+    NUM_SEQUENCES = int(DEV_SEQUENCES.shape[0] / HEIGHT)
 
     model = torch.load("models/CNN.pt")
+    model.to(device)
     predictions = []
 
-    for i in range(0, TEST_SEQUENCES.shape[0], HEIGHT):
-        x = torch.from_numpy(TEST_SEQUENCES[i : i + HEIGHT].astype(np.float32))
+    for i in range(0, DEV_SEQUENCES.shape[0], HEIGHT):
+        x = torch.from_numpy(DEV_SEQUENCES[i : i + HEIGHT].astype(np.float32)).to(
+            device
+        )
         logits = model(x)
-        
+
         pred_normalized = torch.softmax(logits, 1)
         pred = torch.max(pred_normalized, 1)[1]
         predictions.append(pred.item())
-        
-        # if i < 100*82:
-        #     print(pred_normalized)
 
     predictions = np.array(predictions)
     np.savetxt("predictions/CNN_predictions.csv", predictions, fmt="%d")
@@ -87,8 +82,8 @@ if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     if MODE == "train":
-        train()
+        train(device)
     elif MODE == "predict":
-        predict()
+        predict(device)
     else:
         raise Exception("Mode not recognized")

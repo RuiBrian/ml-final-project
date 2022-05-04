988ec7fda7f5699237d1c835fe18e37cd7692f46
Brian Rui <Brian.Rui01@gmail.com>
2022-05-04 17:52:50 -0400

    Formatting

--- main_SVM.py
+++ main_SVM.py
@@ -12,48 +12,52 @@ from sklearn.calibration import CalibratedClassifierCV
 def fit_predict(num_NT):
     # Load training data and labels
     X_train = pd.read_csv(
-        "datasets/processed/{}nt_train_separated.csv".format(num_NT), 
-        header=None, index_col=False)
+        f"datasets/processed/{num_NT}nt_train_separated.csv",
+        header=None,
+        index_col=False,
+    )
     X_train = X_train.to_numpy()
     X_train = OneHotEncoder().fit_transform(X_train).astype(int).toarray()
-    y_train = np.load(
-        "datasets/processed/{}nt_train_labels.npy".format(num_NT)).ravel()
+    y_train = np.load(f"datasets/processed/{num_NT}nt_train_labels.npy").ravel()
 
     # Load dev data and labels
     X_dev = pd.read_csv(
-        "datasets/processed/{}nt_dev_separated.csv".format(num_NT), 
-        header=None, index_col=False)
+        f"datasets/processed/{num_NT}nt_dev_separated.csv",
+        header=None,
+        index_col=False,
+    )
     X_dev = X_dev.to_numpy()
     X_dev = OneHotEncoder().fit_transform(X_dev).astype(int).toarray()
-    y_dev = np.load(
-        "datasets/processed/{}nt_dev_labels.npy".format(num_NT)).ravel()
+    y_dev = np.load(f"datasets/processed/{num_NT}nt_dev_labels.npy").ravel()
 
     # Fit SVM
-    classifier = LinearSVC(C=1, random_state=0, dual=False)
-    model = CalibratedClassifierCV(classifier) 
-    classifier.fit(X_train, y_train)
-    model.fit(X_train, y_train)
-    y_pred = classifier.predict(X_dev)
-    y_softpred = model.predict_proba(X_dev)
+    clf = LinearSVC(C=1, random_state=0, dual=False)
+    calibrated_clf = CalibratedClassifierCV(clf)
+
+    clf.fit(X_train, y_train)
+    calibrated_clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_dev)
+    y_softpred = calibrated_clf.predict_proba(X_dev)
 
     # Save predictions and print accuracy
     np.savetxt(
-        "predictions/{}nt_SVM_dev_predictions.csv".format(num_NT),
+        f"predictions/{num_NT}nt_SVM_dev_predictions.csv",
         y_pred,
         fmt="%i",
-        )
+    )
     np.savetxt(
-        "predictions/{}nt_SVM_dev_softpredictions.csv".format(num_NT),
+        f"predictions/{num_NT}nt_SVM_dev_softpredictions.csv",
         y_softpred,
         fmt="%f",
-        )
+    )
     print(f"Accuracy: {metrics.accuracy_score(y_dev, y_pred)}")
 
 
 if __name__ == "__main__":
+    num_NT = 80
+
     if len(sys.argv) == 2:
         num_NT = sys.argv[1]
-    elif len(sys.argv) == 1:
-        num_NT = 80
-    
+
     fit_predict(num_NT)

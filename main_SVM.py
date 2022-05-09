e5ad4b147838fcc3c89844c74b05a85d92961357
Carolyna Yamamoto Alves Pinto <carolynayamamoto@gmail.com>
2022-05-09 00:24:16 -0400

    learning curve SVM

--- main_SVM.py
+++ main_SVM.py
@@ -44,7 +44,6 @@ def fit_predict(flanking_seq, dataset):
 
     # Plot learning curve
     plot_learning_curve(clf, f"SVM Learning Curves {flanking_seq}nt", X_train, y_train)
-    plt.figure(figsize=(15, 5))
     plt.savefig(f"logs/{flanking_seq}nt_traindev_SVM.png")
     # plt.show()
 
@@ -67,89 +66,47 @@ def plot_learning_curve(
     title,
     X,
     y,
-    axes=None,
-    ylim=None,
-    cv=None,
-    train_sizes=np.linspace(0.1, 1.0, 5),
 ):
-
-    if axes is None:
-        _, axes = plt.subplots(1, 3, figsize=(20, 5))
-
-    axes[0].set_title(title)
-    if ylim is not None:
-        axes[0].set_ylim(*ylim)
-    axes[0].set_xlabel("Training examples")
-    axes[0].set_ylabel("Score")
+    
+    plt.figure(figsize=(15, 5))
+    plt.title(title)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
 
     train_sizes, train_scores, test_scores, fit_times, _ = learning_curve(
         estimator,
         X,
         y,
-        cv=cv,
-        train_sizes=train_sizes,
         return_times=True,
     )
     train_scores_mean = np.mean(train_scores, axis=1)
     train_scores_std = np.std(train_scores, axis=1)
     test_scores_mean = np.mean(test_scores, axis=1)
     test_scores_std = np.std(test_scores, axis=1)
-    fit_times_mean = np.mean(fit_times, axis=1)
-    fit_times_std = np.std(fit_times, axis=1)
 
     # Plot learning curve
-    axes[0].grid()
-    axes[0].fill_between(
+    plt.grid()
+    plt.fill_between(
         train_sizes,
         train_scores_mean - train_scores_std,
         train_scores_mean + train_scores_std,
         alpha=0.1,
-        color="r",
+        color="b",
     )
-    axes[0].fill_between(
+    plt.fill_between(
         train_sizes,
         test_scores_mean - test_scores_std,
         test_scores_mean + test_scores_std,
         alpha=0.1,
-        color="g",
+        color="r",
     )
-    axes[0].plot(
-        train_sizes, train_scores_mean, "o-", color="r", label="Training score"
+    plt.plot(
+        train_sizes, train_scores_mean, "o-", color="b", label="Training score"
     )
-    axes[0].plot(
-        train_sizes, test_scores_mean, "o-", color="g", label="Cross-validation score"
+    plt.plot(
+        train_sizes, test_scores_mean, "o-", color="r", label="Cross-validation score"
     )
-    axes[0].legend(loc="best")
-
-    # # Plot n_samples vs fit_times
-    # axes[1].grid()
-    # axes[1].plot(train_sizes, fit_times_mean, "o-")
-    # axes[1].fill_between(
-    #     train_sizes,
-    #     fit_times_mean - fit_times_std,
-    #     fit_times_mean + fit_times_std,
-    #     alpha=0.1,
-    # )
-    # axes[1].set_xlabel("Training examples")
-    # axes[1].set_ylabel("fit_times")
-    # axes[1].set_title("Scalability of the model")
-
-    # # Plot fit_time vs score
-    # fit_time_argsort = fit_times_mean.argsort()
-    # fit_time_sorted = fit_times_mean[fit_time_argsort]
-    # test_scores_mean_sorted = test_scores_mean[fit_time_argsort]
-    # test_scores_std_sorted = test_scores_std[fit_time_argsort]
-    # axes[2].grid()
-    # axes[2].plot(fit_time_sorted, test_scores_mean_sorted, "o-")
-    # axes[2].fill_between(
-    #     fit_time_sorted,
-    #     test_scores_mean_sorted - test_scores_std_sorted,
-    #     test_scores_mean_sorted + test_scores_std_sorted,
-    #     alpha=0.1,
-    # )
-    # axes[2].set_xlabel("fit_times")
-    # axes[2].set_ylabel("Score")
-    # axes[2].set_title("Performance of the model")
+    plt.legend(loc="best")
 
     return plt
 
